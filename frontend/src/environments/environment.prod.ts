396e211049eb26b5d9724cd885fa98d4e7189e33
taqil <phonrat333@gmail.com>
2024-08-22 11:05:45 +0700

    Initial commit

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    dotnet_api_url: 'http://localhost:8081/api/',
+    dotnet_api_url_image: 'http://localhost:8081/uploads/',
+    base_url: '',
+    token: '',
+    version: '1.0',
+}
